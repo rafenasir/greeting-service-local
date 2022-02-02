d839bc59443e208b8da126911f0a11da373ead6c
Rafe Nasir <70711865+rafenasir@users.noreply.github.com>
2022-02-02 10:55:20 +0100

    Updated authorization in the client and right base path

--- GreetingService.Core/Entities/Greeting.cs
+++ GreetingService.Core/Entities/Greeting.cs
@@ -5,7 +5,7 @@
         public string Message { get; set; }
         public string From { get; set; }
         public string To { get; set; }
-        public DateTime Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }= DateTime.Now;
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public virtual string GetMessage()

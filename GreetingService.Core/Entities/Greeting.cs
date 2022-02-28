e3d71a1fad4ff621f03bcf29e096e7c2aae7d40d
Rafe Nasir <70711865+rafenasir@users.noreply.github.com>
2022-02-28 14:24:32 +0100

    updated validator functions

--- GreetingService.Core/Entities/Greeting.cs
+++ GreetingService.Core/Entities/Greeting.cs
@@ -1,11 +1,37 @@
-﻿namespace GreetingService.Core.Entities
+﻿using GreetingService.Core.Exceptions;
+using GreetingService.Core.HelperFunctions;
+
+namespace GreetingService.Core.Entities
 {
     public class Greeting
     {
         public string Message { get; set; }
-        public string From { get; set; }
-        public string To { get; set; }
-        public DateTime Timestamp { get; set; }= DateTime.Now;
+        private string _from;
+        public string From
+        {
+            get
+            {
+                return _from;
+            }
+            set
+            {
+                if (!EmailValidator.IsValidEmail(value))               
+                    throw new InvalidEmailException($"{value} is not a valid email address");
+                _from = value;
+            }
+        }
+        private string _to;
+        public string To { get
+            {
+                return _to;
+            }
+            set {
+                if (!EmailValidator.IsValidEmail(value))
+                    throw new InvalidEmailException($"{value} is not a valid email address");
+                _to = value;   
+            } 
+        }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
         public Guid Id { get; set; } = Guid.NewGuid();
 
         public virtual string GetMessage()
---
+++ GreetingService.Core/HelperFunctions/EmailValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GreetingService.Core.HelperFunctions
+{
+    public class EmailValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+
+            try
+            {
+                if (string.IsNullOrEmpty(email)) { return false; }
+
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                          RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch(RegexMatchTimeoutException ex) { return false; }
+            catch(ArgumentException ex) { return false; }
+
+            try
+            {
+                return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}

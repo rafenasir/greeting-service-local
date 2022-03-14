4e2f5a30922fd38e21dbb598a2c9ad4c323ff003
Rafe Nasir <70711865+rafenasir@users.noreply.github.com>
2022-03-14 13:50:48 +0100

    udpated email verfication in user entity

--- GreetingService.Core/HelperFunctions/EmailValidator.cs
+++ GreetingService.Core/HelperFunctions/EmailValidator.cs
@@ -10,29 +10,45 @@ namespace GreetingService.Core.HelperFunctions
 {
     public class EmailValidator
     {
+
+
         public static bool IsValidEmail(string email)
         {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
 
             try
             {
-                if (string.IsNullOrEmpty(email)) { return false; }
-
+                // Normalize the domain
                 email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
-                                          RegexOptions.None, TimeSpan.FromMilliseconds(200));
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
 
+                // Examines the domain part of the email and normalizes it.
                 string DomainMapper(Match match)
                 {
+                    // Use IdnMapping class to convert Unicode domain names.
                     var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
                     string domainName = idn.GetAscii(match.Groups[2].Value);
+
                     return match.Groups[1].Value + domainName;
                 }
             }
-            catch(RegexMatchTimeoutException ex) { return false; }
-            catch(ArgumentException ex) { return false; }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
 
             try
             {
-                return Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
             }
             catch (RegexMatchTimeoutException)
             {
@@ -40,4 +56,5 @@ namespace GreetingService.Core.HelperFunctions
             }
         }
     }
+
 }

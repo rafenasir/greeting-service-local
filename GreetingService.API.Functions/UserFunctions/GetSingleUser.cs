1fbb3d13c7fd9a1e458bb0c4f7fff7f77d1b3bca
Rafe Nasir <70711865+rafenasir@users.noreply.github.com>
2022-03-14 13:33:47 +0100

    updated, user endpoints

---
+++ GreetingService.API.Functions/UserFunctions/GetSingleUser.cs
@@ -0,0 +1,49 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using GreetingService.API.Functions.Authentication;
+using GreetingService.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace GreetingService.API.Functions.UserFunctions
+{
+    public class GetSingleUser
+    {
+        private readonly ILogger<GetSingleUser> _logger;
+        private readonly IAuthHandler _authHandler;
+        private readonly IUserService _userService;
+        public GetSingleUser(ILogger<GetSingleUser> log, IAuthHandler authHandler, IUserService userService)
+        {
+            _logger = log;
+            _authHandler = authHandler;
+            _userService = userService;
+        }
+
+        [FunctionName("GetSingleUser")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "User" })]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user/{email}")] HttpRequest req, string email)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (!_authHandler.IsAuthorized(req))
+                return new UnauthorizedResult();
+
+            var user = await _userService.GetUserAsync(email);
+
+            if (user == null)
+                return new NotFoundObjectResult("Not found");
+
+            return new OkObjectResult(user);
+        }
+    }
+}
+
